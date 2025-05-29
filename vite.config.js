3d838e9f57f8db0e7ae060c87cc07dd5bc450ad5
Aashish Khadka <aashishkhadka720@gmail.com>
2025-05-29 23:15:40 +0545

    Refactor Vercel and Vite configuration for improved routing and plugin integration

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: 'dist', // Ensure the output directory is correctly set
-    },
-    server: {
-        port: 3000, // Optional: Set the local development port
-    },
-    base: './', // Ensure relative paths for assets
-});
+  plugins: [react(), tailwindcss(),],
+  server: {
+    host: true,
+    port: 5173,
+  },
+})
 
